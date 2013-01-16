249c62127da2c28ea1d5ebd6765a72dba15088e3
Unai Aguilera <gkalgan@gmail.com>
2013-01-16 16:04:30 +0100

    Echo added to database creation

--- ckanext/metadata/model/initDB.py
+++ ckanext/metadata/model/initDB.py
@@ -1,6 +1,7 @@
 import property_model
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://ckanuser:pass@localhost/ckantest')
+print 'Creating table for metadata property storage'
+engine = create_engine('postgresql://ckanuser:pass@localhost/ckantest', echo=True)
 property_model.metadata.drop_all(engine)
 property_model.metadata.create_all(engine)
