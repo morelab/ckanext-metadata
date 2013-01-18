1234d53f7bd63d9231bb12b42e900b583a07b470
Unai Aguilera <gkalgan@gmail.com>
2013-01-18 15:27:55 +0100

    Tabulation normalized to 4 spaces

--- setup.py
+++ setup.py
@@ -4,35 +4,35 @@ import sys, os
 version = '0.1'
 
 setup(
-	name='ckanext-metadata',
-	version=version,
-	description="SPARQL endpoint analyzer and metadata generator for CKAN",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Unai Aguilera',
-	author_email='unai.aguilera@deusto.es',
-	url='http://www.morelab.deusto.es',
-	license='AGPL',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.metadata'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		'celery==2.4.2',
-		'requests',
-		'swanalyzer'
-	],
-	dependency_links=[
-    	"https://github.com/memaldi/SWAnalyzer/tarball/master#egg=swanalyzer",
+    name='ckanext-metadata',
+    version=version,
+    description="SPARQL endpoint analyzer and metadata generator for CKAN",
+    long_description="""\
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Unai Aguilera',
+    author_email='unai.aguilera@deusto.es',
+    url='http://www.morelab.deusto.es',
+    license='AGPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.metadata'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'celery==2.4.2',
+        'requests',
+        'swanalyzer'
     ],
-	entry_points=\
-	"""
-	[ckan.plugins]
-		metadata=ckanext.metadata.plugin:MetadataExtension
-		
-	[ckan.celery_task]
-		tasks = ckanext.metadata.celery_import:task_imports
-	""",
+    dependency_links=[
+        "https://github.com/memaldi/SWAnalyzer/tarball/master#egg=swanalyzer",
+    ],
+    entry_points=\
+    """
+    [ckan.plugins]
+        metadata=ckanext.metadata.plugin:MetadataExtension
+        
+    [ckan.celery_task]
+        tasks = ckanext.metadata.celery_import:task_imports
+    """,
 )
